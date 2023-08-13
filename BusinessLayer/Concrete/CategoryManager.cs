310bc1f974aaf2797245eee4093af996bfefa3fa
Ertugrul <57365047+officialerto@users.noreply.github.com>
2023-08-13 22:43:58 +0300

    MVC_Proje_SON

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,36 +10,32 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
+        ICategoryDal _categorydal; 
+        public List<Category> GetList()
+        {
+            return 
+        }
+
+
         //REPO ADI ALTINDA NESNE TÜRETİYORUZ
-        GenericRepository<Category> repo = new GenericRepository<Category>();
-
-        public List<Category> GetAllBL()
-        {
-
-            return repo.List();
-
-        }
-
-        public void CategoryAddBL(Category p)
-        {
-
-            if(p.CategoryName==""  || p.CategoryName.Length <= 3 || p.CategoryDescription=="" || p.CategoryName.Length>=51)
-            {
-
-                //HATA MESAJI
-
-            }
-            else
-            {
-
-                repo.Insert(p);
-
-            }
-
-        }
-
+        //GenericRepository<Category> repo = new GenericRepository<Category>();
+        //public List<Category> GetAllBL()
+        //{
+        //    return repo.List();
+        //}
+        //public void CategoryAddBL(Category p)
+        //{
+        //    if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length >= 51)
+        //    {
+        //        //HATA MESAJI
+        //    }
+        //    else
+        //    {
+        //        repo.Insert(p);
+        //    }
+        //}
 
     }
 }
--- MVCProjeKampi/Controllers/CategoryController.cs
+++ MVCProjeKampi/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,33 @@ namespace MVCProjeKampi.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager();
+
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult GetCatgoryList()
+            {
+            //var categoryvalues = cm.GetAllBL();
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+
+            return RedirectToAction("GetCatgoryList");
+        }
+    
     }
 }
